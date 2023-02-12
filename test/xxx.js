0ffba0ddd6e4d5483a699d72d2fcac8f16728774
fedeghe <fedeghe@gmail.com>
2023-02-12 22:48:38 +0100

    up

--- test/xxx.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert')
-var jscompare = require('../dist/index.js')
-
-describe('hello', () => {
-    it('do the job', () => {
-        assert(jscompare('a', 'a'))
-    })
-})
