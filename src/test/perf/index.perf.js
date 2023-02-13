e37c17a6a7698d8f3ffb13c646c6516dae94a107
fedeghe <fedeghe@gmail.com>
2023-02-13 08:34:50 +0100

    seems like there's a preliminary clear winner agains the trivial stringify

--- src/test/perf/index.perf.js
+++ src/test/perf/index.perf.js
@@ -1,14 +1,37 @@
 var assert = require('assert'),
     testone = require('@fedeghe/testone'),
-    jscompare = require('../../dist/index.js');
-
+    jscompare = require('../../dist/index.js'),
+    strat = require('./strat');
+var o = [
+    {
+        a:'s',
+        b: o
+    },
+    {r:1},
+    [1,2,3],
+    1,
+    '2',
+    true,
+    false,
+]
 var r = testone(
     [{
         in: ['a', 'a'],
         out: true
+    },
+    {
+        in: [o, o],
+        out: true
     }],
-    [jscompare]
+    [jscompare, ...strat],
+    {
+        iterations: 1E6,
+        metrics: {fx:({mem, time}) => time*mem}
+    }
 )
-// console.log(r)
+// console.log(JSON.stringify(r, null, 2))
+console.log(r)
 assert(r.passing.jscompare)
+assert(r.passing.object_equals)
+assert(r.passing.areEquivalent)
 
